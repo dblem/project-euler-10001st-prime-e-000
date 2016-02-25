002a52706ae1864f1197a412a8ada79aac4f1055
dblem <dax.blem@gmail.com>
2016-02-25 00:11:34 +0000

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,21 @@
-# Implement your procedural solution here!
+def is_prime?(x)
+  x_root = Math.sqrt(x).to_i
+  (2..x_root).all? do |y|
+    x % y != 0
+  end
+end
+
+def prime_number_for(nth_element)
+  n = nth_element.to_i
+  prime_count = 2
+  num = 3
+  nth_prime = 3
+  until prime_count == n
+    num += 2
+    if is_prime?(num)
+      nth_prime = num
+      prime_count += 1
+    end
+  end
+  nth_prime
+end
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,28 @@
-# Implement your object-oriented solution here!
+class Prime
+
+  def initialize(input)
+    @nth_element = input.to_i
+    @prime_count = 2
+    @num = 3
+    @nth_prime = 3
+  end
+
+  def is_prime?
+    num_root = Math.sqrt(@num).to_i
+    (2..num_root).all? do |x|
+      @num % x != 0
+    end
+  end
+
+  def number
+    until @prime_count == @nth_element
+      @num += 2
+      if is_prime?
+        @nth_prime = @num
+        @prime_count += 1
+      end
+    end
+    @nth_prime
+  end
+
+end
